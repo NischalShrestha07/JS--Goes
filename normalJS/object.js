3d81fcbb7b27aaaa172947b263e28a0b150415da
nischal <nischalshrestha800@gmail.com>
2024-04-14 22:35:54 -0700

    .

---
+++ normalJS/object.js
@@ -0,0 +1,13 @@
+const car = {
+    model: "Mustang",
+    color: "red",
+    year: "2024",
+
+    drive: function() { console.log("You drive the car"); },
+    brake: function() { console.log("You steo on the brake"); }
+}
+console.log(car.model);
+console.log(car.color);
+console.log(car.year);
+car.drive()
+car.brake()
